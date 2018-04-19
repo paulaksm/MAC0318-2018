1c5571bcf8830675d7627eda3956258049266cf5
Paula Moraes <30325208+paulaksm@users.noreply.github.com>
2018-04-19 16:16:07 -0300

    Update install.sh

--- dotfiles/rpi/install.sh
+++ dotfiles/rpi/install.sh
@@ -6,10 +6,9 @@ sudo apt-get -y purge wolfram-engine
 # Installing opencv-3.3.0 on python3
 sudo apt-get -y install build-essential git cmake pkg-config
 sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
-sudo apt-get -y install libgtk2.0-dev libgtk-3-dev
+sudo apt-get -y install libgtk2.0-dev 
 sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get -y install libxvidcore-dev libx264-dev
-sudo apt-get -y install libgtk2.0-dev
 sudo apt-get -y install libatlas-base-dev gfortran
 cd ~
 wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
@@ -25,6 +24,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules \
     -D BUILD_EXAMPLES=ON \
+    -D WITH_GTK=ON \
     -D WITH_JPEG=OFF ..
 make -j2
 sudo make install
